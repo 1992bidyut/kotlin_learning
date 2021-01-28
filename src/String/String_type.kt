94b617b499006ef7026abdbf171b7805f5ee425a
Bidyut Debnath <1992bidyut@gmail.com>
2021-01-29 02:08:02 +0600

    string

--- src/String/String_type.kt
+++ src/String/String_type.kt
@@ -1,2 +1,35 @@
 package String
 
+fun main(){
+    val greeting: String = "Hello there"
+    val owe = 50
+
+    val janet = "I owe Janet \$$owe dollars"
+    println(janet)
+
+    val infinity = "The infinity symbol is \u221E"
+    println(infinity)
+
+    println("[$greeting] is ${greeting.length} characters long")
+
+    var bad = greeting.get(2)
+    val letter = greeting[1]
+
+    val compare1 = "beta"
+    val compare2 = "alpha"
+    println("${compare1.compareTo(compare2)}")
+
+    val sub = greeting.subSequence(6,9)
+    println("Sub = $sub")
+
+    for (single in greeting){
+        print("$single ")
+    }
+
+    //row string
+    val myString = """
+        Kotlin is fun
+        Kotlin is pragmatic
+    """
+    println("$myString")
+}
